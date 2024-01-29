04c08a325b02549433579fabb54f04d697aba76c
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-01-29 21:14:42 +0200

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,6 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { ResetTv } from '@mui/icons-material';
 
 const defaultTheme = createTheme();
 
