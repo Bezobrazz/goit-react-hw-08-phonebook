9e60cc60c47fcc3a67cce64fc9dea387d915006c
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-01-29 22:13:29 +0200

    error with register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,10 +8,14 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useDispatch } from 'react-redux';
+import { registerThunk } from '../redux/operations';
 
 const defaultTheme = createTheme();
 
 export default function Register() {
+  const dispatch = useDispatch();
+
   const handleSubmit = event => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -20,6 +24,7 @@ export default function Register() {
       email: data.get('email'),
       password: data.get('password'),
     });
+    dispatch(registerThunk(data));
     event.currentTarget.reset();
   };
 
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://65b264219bfb12f6eafda298.mockapi.io/';
+// axios.defaults.baseURL = 'https://65b264219bfb12f6eafda298.mockapi.io/';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
@@ -32,6 +34,18 @@ export const addContactsThunk = createAsyncThunk(
   async (body, thunkAPI) => {
     try {
       const { data } = await axios.post(`contacts`, body);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const registerThunk = createAsyncThunk(
+  'register',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post('users/signup', credentials);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
